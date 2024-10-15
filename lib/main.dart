6f2880fbf3d4ddc3583ed39285deb1ccc5290634
vanvixi <vanvixi.dev@gmail.com>
2024-10-15 14:48:42 +0700

    Add package_info_plus library to read package information

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:package_info_plus/package_info_plus.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(const MyApp());
 }
 
@@ -30,12 +32,11 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  PackageInfo? packageInfo;
 
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
+  Future<void> _getInfo() async {
+    packageInfo = await PackageInfo.fromPlatform();
+    setState(() {});
   }
 
   @override
@@ -49,20 +50,21 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
+            Text('AppName: ${packageInfo?.appName}'),
+            const SizedBox(height: 4),
+            Text('PackageName: ${packageInfo?.packageName}'),
+            const SizedBox(height: 4),
+            Text('Version: ${packageInfo?.version}'),
+            const SizedBox(height: 4),
+            Text('BuildNumber: ${packageInfo?.buildNumber}'),
+            const SizedBox(height: 4),
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+        onPressed: _getInfo,
+        tooltip: 'Get info',
+        child: const Icon(Icons.info),
       ),
     );
   }
